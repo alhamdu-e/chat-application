ee3b7adabf4d81e9db9512ec862aaa54b103f3ad
alhamdu-e <alhamdu7624@gmail.com>
2024-08-02 09:25:10 +0300

    database connection created

--- Backend/index.js
+++ Backend/index.js
@@ -3,11 +3,19 @@ const cors = require("cors");
 const bodyParser = require("body-parser");
 const morgan = require("morgan");
 const app = express();
+const userRoutes = require("./routes/user.js");
+const monogoConnect = require("./utils/database.js");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cors());
+app.use(morgan("dev"));
 
-app.listen(5000, () => {
-	console.log("server started 5000");
+app.use("/users", userRoutes);
+
+monogoConnect((client) => {
+	console.log(client);
+	app.listen(5000, () => {
+		console.log("server started 5000");
+	});
 });
